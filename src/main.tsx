6d432d7bae79383cb2e8e47a43fe4459d27e52d0
salua <sophiemorales6440@gmail.com>
2026-01-14 09:42:02 +0100

    2nd commit change main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Link, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// page components
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// router creation
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <main>
+          <Home />
+        </main>
+      </>
+    ),
+  },
+  {
+    path: "/about",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <main>
+          <About />
+        </main>
+      </>
+    ),
+  },
+]);
+
+// rendering
+
+const rootElement = document.getElementById("root");
+
+if (rootElement != null) {
+  ReactDOM.createRoot(rootElement).render(
+    <RouterProvider router={router} />
+  );
+}
